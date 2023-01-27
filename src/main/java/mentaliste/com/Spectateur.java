6187c279adcfe6d797f5fbb4cf669f3f1ec7be6b
MaxouD1 <maxence.drozdz.ecole@gmail.com>
2023-01-27 13:55:28 +0100

    modif 1

--- src/main/java/mentaliste/com/Spectateur.java
+++ src/main/java/mentaliste/com/Spectateur.java
@@ -10,10 +10,18 @@ public class Spectateur {
         Scanner scanner = new Scanner(System.in);
         System.out.println("[Spectateur] J'entre mon âge : ");
         this.age = scanner.nextInt();
+        while (true) {
+            if (age > 99 && age <= 0) {
+                System.out.println("[Spectateur] J'entre mon âge(max:99) : ");
+                this.age = scanner.nextInt();
+            } else {
+                break;
+            }
+        }
         System.out.println("[Spectateur] J'entre ma somme : ");
         this.somme = scanner.nextInt();
         while (true) {
-            if (somme > 99) {
+            if (somme > 99 && somme <= 0) {
                 System.out.println("[Spectateur] J'entre ma somme (max:99) : ");
                 this.somme = scanner.nextInt();
             } else {
